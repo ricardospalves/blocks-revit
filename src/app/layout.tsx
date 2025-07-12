521c015e686388d3635a3873d193c84161bb81a9
Ricardo Alves <ricardospalves@gmail.com>
2025-07-12 14:31:52 -0300

    chore: add Open Sans font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,12 @@
 import type { Metadata } from 'next'
+import { Open_Sans } from 'next/font/google'
 import './globals.css'
+
+const openSans = Open_Sans({
+  variable: '--font-open-sans',
+  subsets: ['latin'],
+  fallback: ['sans-serif'],
+})
 
 export const metadata: Metadata = {
   title: 'Blocks Revit',
@@ -16,7 +23,9 @@ export default function RootLayout({
         <meta name="apple-mobile-web-app-title" content="Blocks Revit" />
       </head>
 
-      <body className="antialiased">{children}</body>
+      <body className={`${openSans.variable} font-normal antialiased`}>
+        {children}
+      </body>
     </html>
   )
 }
